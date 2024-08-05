555e0651a8810a93801820a10c458f06e118f6ae
xuan bach <benlam05032004@gmail.com>
2024-08-05 14:23:18 +0700

    example on how numpy shortens code

--- nnfs.py
+++ nnfs.py
@@ -12,5 +12,12 @@ for n_weight, n_bias in zip(weights, bias):
     n_output += n_bias
     layer_output.append(n_output)
 
-print(layer_output) 
+print(layer_output)
 
+#doing the same thing with numpy
+
+input = [0.1,0.2,0.3,0.4]
+weights = [[0.2,0.1,0.3,0.4],[0.4,0.6,0.1,0.9],[0.7,0.6,0.1,0.1]]
+bias = [0.2,0.3,0.4]
+
+n_output = np.dot(weights, input)+bias
