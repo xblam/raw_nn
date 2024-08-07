a0d381d157187e003a3e886c1dcbd804fdb2975c
xuan bach <benlam05032004@gmail.com>
2024-08-07 08:46:05 +0700

    failed commit

--- my_nnfs.py
+++ my_nnfs.py
@@ -15,8 +15,24 @@ plt.scatter(X[:,0],X[:,1], c=y, cmap='brg')
 # input('hit any key to end: ')
 # plt.close('all')
 
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 class Dense_layer:
     def __init__(self, n_inputs, n_neurons):
-        pass
-    def forward(self, n_inputs):
-        pass
+        self.weights = 0.01*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons)) # we want all of them to be matrices instead of just arrays
+    
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+if __name__ == '__main__':
+    X, y = spiral_data(samples=100, classes=3)
+
+    dense1 = Dense_layer(2,3)
+    activation = Activation_ReLU()
+
+    dense1.forward(X)
+    activation.forward(dense1.output)
+    print(activation.output[:5])
